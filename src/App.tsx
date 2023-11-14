129ea7567006d1fcfd05a29d3aa94aae0662bf73
vannguyen5778 <hoai.van@bk.ru>
2023-11-14 22:31:27 +0700

    Add static Categories section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import '@/scss/App.scss'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-
+// import Button from './components/Button'
+import ArrowUp from "@/assets/img/arrow-top.svg"
 function App() {
-  
+  const category = ["All", "Vegeterian", "Protein"]
   return (
     <div className='app'>
       <Header />
+      <div className="top-section">
+        <ul className='categories'>
+        {category.map(el =>
+        <li className="categories-btn" key={el}>{el}</li>
+        )}
+        </ul>
+
+        <div className="sort-wrapper">
+          <div className="sort">
+            <img src={ArrowUp} alt="arrow up" />
+            <span className='sort__text'>Сортировка по: </span>
+            <span className='sort__choice'>популярности</span>
+          </div>
+          <div className="sort-popup">
+            <ul>
+              <li>dsfsf</li>
+              <li>dsfsf</li>
+              <li>dsfsf</li>
+            </ul>
+          </div>
+        </div>
+
+      </div>
       <Routes>
         {/* <Route path="/" element={}></Route> */}
       </Routes>
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    children: string;
+}
+
+function Button({ children }: Props) {
+  return (
+    <li className="categories-btn">{children}</li>
+  )
+}
+
+export default Button
